9331422e8978307fd00bcac55f8ea4b5d8000456
Eduardo Pinto <eduardo.pinto@blip.pt>
2018-05-08 17:32:52 +0100

    postfix working

--- src/main/scala/com/cutajarjames/scalabyexample/lesson6/ShuntingYard.scala
+++ src/main/scala/com/cutajarjames/scalabyexample/lesson6/ShuntingYard.scala
@@ -4,9 +4,7 @@ package com.cutajarjames.scalabyexample.lesson6
 
 class ShuntingYard {
 
-
-  val opStack = List("/", "*", "+", "-")
-
+  val allOps = List("/", "*", "+", "-")
 
   def evaluateInfix(infix:String): Double =  evaluatePostfix(toPostfix(infix.split(" ").toList))
 
@@ -19,7 +17,20 @@ class ShuntingYard {
       (tuple, token) =>
         val (postFix, opStack) = tuple
         token match {
+          case t if isNumber(t) => (postFix :+ t, opStack)
 
+          case t if isOperator(t) =>
+            val higherOps = allOps.span(_ != t)._1
+            val ops = opStack.span(higherOps.contains)
+            (postFix ::: ops._1, t +: ops._2)
+
+          case t if isLeftBrace(t) => (postFix, t +: opStack)
+
+          case t if isRightBrace(t) =>
+            val split = opStack.span(_ != "(")
+            (postFix ::: split._1, split._2.tail)
+
+          case _ => throw new Exception("Just respect the rules dude")
         }
     }
 
@@ -28,5 +39,10 @@ class ShuntingYard {
   }
 
   def evaluatePostfix(postFix:List[String]): Double = 0.0
+
+  private def isNumber(str: String) = str forall Character.isDigit
+  private def isOperator(str: String) = allOps.contains(str)
+  private def isLeftBrace(str: String) = str == "("
+  private def isRightBrace(str: String) = str == ")"
 
 }
