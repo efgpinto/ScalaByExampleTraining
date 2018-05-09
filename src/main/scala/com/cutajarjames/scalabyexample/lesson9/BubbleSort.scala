54ce25bdd87e3f215b3b9c84edd34d65d52b606c
Eduardo Pinto <eduardo.pinto@blip.pt>
2018-05-09 14:31:42 +0100

    bubbling

--- src/main/scala/com/cutajarjames/scalabyexample/lesson9/BubbleSort.scala
+++ src/main/scala/com/cutajarjames/scalabyexample/lesson9/BubbleSort.scala
@@ -1,7 +1,17 @@
 package com.cutajarjames.scalabyexample.lesson9
 
 class BubbleSort {
-  def sort(numbers: Array[Int]): Unit = {
 
-  }
+  def sort(numbers: Array[Int]): Unit =
+    for (k <- 1 until numbers.length;
+         j <- 0 until numbers.length - k
+         if numbers(j) > numbers(j + 1)) {
+
+      // cool swap
+      numbers(j) += numbers(j + 1)
+      numbers(j + 1) = numbers(j) - numbers(j + 1)
+      numbers(j) -= numbers(j + 1)
+
+    }
+
 }
