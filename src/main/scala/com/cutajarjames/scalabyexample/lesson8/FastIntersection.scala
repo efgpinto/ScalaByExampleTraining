24382e19e5016d46a63038a86a0f08e8c78df44d
Eduardo Pinto <eduardo.pinto@blip.pt>
2018-05-09 14:11:45 +0100

    sorting problems

--- src/main/scala/com/cutajarjames/scalabyexample/lesson8/FastIntersection.scala
+++ src/main/scala/com/cutajarjames/scalabyexample/lesson8/FastIntersection.scala
@@ -1,8 +1,15 @@
 package com.cutajarjames.scalabyexample.lesson8
 
 class FastIntersection {
-  def intersection(x: List[Int], y: List[Int]): List[Int] = {
-    null
+  def intersection(x: List[Int], y: List[Int]): List[Int] = intersectionSorted(x.sorted, y.sorted)
+
+  def intersectionSorted(x: List[Int],
+                         y: List[Int],
+                         acum: List[Int] = Nil): List[Int] = (x, y) match {
+    case (Nil, _) | (_, Nil) => acum
+    case (x1 :: xt, y1 :: yt) if x1 == y1 => intersectionSorted(xt, yt, acum :+ x1)
+    case (x1 :: xt, y1 :: _) if x1 < y1 => intersectionSorted(xt, y, acum)
+    case _ => intersectionSorted(x, y.tail, acum)
   }
 
 }
